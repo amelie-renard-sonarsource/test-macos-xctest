d2510367417f1066d1a9b8f75eb46fa2e80d636d
amelie-renard-sonarsource <amelie.renard@sonarsource.com>
2022-04-29 14:44:01 +0200

    Initial Commit

---
+++ test-macos-xctest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test-macos-xctest
+//
+//  Created by Amélie Renard on 29.04.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
